e5239876f3ac659457dedfc2579d4a7ea74401fe
mikailerkart <99979466+mikailerkart@users.noreply.github.com>
2022-03-29 11:39:26 +0200

    discordbot
    init

---
+++ commands/dropdown13.js
@@ -0,0 +1,54 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    if(!message.member.roles.cache.has('886602905468158003')) /*anket kurma yetkili rol */ return message.reply("Anket kurmak için yetkin yok.");
+
+    const options = [
+    {
+
+        label: `Güzel sanatlar`,
+        value: "958296041021644820",
+        emoji: "🎨"
+    },
+    {
+
+        label: `Animasyon tasarım`,
+        value: "958296410921533460",
+        emoji: "👨‍💻"
+    },
+    {
+
+        label: `Fizyoterapi ve rehabilitasyonm`,
+        value: "958297263258619967",
+        emoji: "💀"
+    },
+    {
+
+        label: "Eczacılık",
+        value: "958297618038009866",
+        emoji: "💊"
+    }
+
+    ];
+
+    
+    const Row = new discord.MessageActionRow()
+    .addComponents(
+        new discord.MessageSelectMenu()
+        .setCustomId("roles")
+        .setMinValues(0)
+        .setMaxValues(4)
+        .setPlaceholder("Bir rol seç.")
+        .addOptions(options)
+    )
+
+    return message.channel.send({components: [Row] });
+
+}
+
+module.exports.help = {
+    name: "dropdown13",
+    category: "general",
+    description: "Anket"
+}
